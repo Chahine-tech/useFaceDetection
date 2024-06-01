f7927c51d1ca174c12cd313bef1f7cfa6e03c069
Chahine-tech <chahine.benlahcen@gmail.com>
2024-06-01 23:05:26 +0200

    feat: update face detection code to use video input

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,50 @@
-use opencv::{highgui, imgcodecs, imgproc, objdetect, prelude::*, types};
+use opencv::{core, highgui, imgproc, objdetect, prelude::*, videoio};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let face_cascade_path = "data/haarcascade_frontalface_default.xml";
-    // Declare `face_cascade` as mutable
     let mut face_cascade = objdetect::CascadeClassifier::new(face_cascade_path)?;
 
-    let image_path = "data/image.jpg";
-    let mut img = imgcodecs::imread(image_path, imgcodecs::IMREAD_COLOR)?;
-
-    let mut faces = types::VectorOfRect::new();
-    // Now `face_cascade` can be borrowed as mutable
-    face_cascade.detect_multi_scale(
-        &img,
-        &mut faces,
-        1.1,
-        3,
-        objdetect::CASCADE_SCALE_IMAGE,
-        opencv::core::Size::new(30, 30),
-        opencv::core::Size::default(),
-    )?;
-
-    for face in faces {
-        imgproc::rectangle(
-            &mut img,
-            face,
-            opencv::core::Scalar::new(0.0, 255.0, 0.0, 0.0),
-            1,
-            imgproc::LINE_8,
-            0,
-        )?;
+    // Ouvrir le flux vidéo
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 correspond à la première caméra
+    if !videoio::VideoCapture::is_opened(&cam)? {
+        panic!("Impossible d'ouvrir la caméra");
     }
 
-    highgui::named_window("Face Detection", highgui::WINDOW_AUTOSIZE)?;
-    highgui::imshow("Face Detection", &img)?;
-    highgui::wait_key(0)?;
+    loop {
+        let mut frame = core::Mat::default();
+        cam.read(&mut frame)?;
+
+        if frame.size()?.width > 0 {
+            let mut faces = core::Vector::<core::Rect>::new();
+            face_cascade.detect_multi_scale(
+                &frame,
+                &mut faces,
+                1.1,
+                3,
+                objdetect::CASCADE_SCALE_IMAGE,
+                opencv::core::Size::new(30, 30),
+                opencv::core::Size::default(),
+            )?;
+
+            for face in faces {
+                imgproc::rectangle(
+                    &mut frame,
+                    face,
+                    opencv::core::Scalar::new(0.0, 255.0, 0.0, 0.0),
+                    1,
+                    imgproc::LINE_8,
+                    0,
+                )?;
+            }
+
+            highgui::imshow("Face Detection", &frame)?;
+        }
+
+        // Quitter la boucle si la touche 'q' est pressée
+        if highgui::wait_key(10)? == 113 {
+            break;
+        }
+    }
 
     Ok(())
 }
